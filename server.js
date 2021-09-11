c3188c97d0f1e36fe60a189c76389e43b880739b
Crystal Lee <crystalleedev@gmail.com>
2021-09-11 16:35:14 -0700

    Added tests, refactored to jsx

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const path = require("path")
-const express = require("express"); // npm installed
+const express = require("express");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
-// other configuration...
 
 app.listen(3000);
