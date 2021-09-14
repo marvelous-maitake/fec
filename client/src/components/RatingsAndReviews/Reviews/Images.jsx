e4ee0f47908f2fa40a159d7298d2d1db43df3865
jshzglr <zigler.josh@icloud.com>
2021-09-14 02:39:41 -0500

    implemented basic image modal

--- client/src/components/RatingsAndReviews/Reviews/Images.jsx
+++ client/src/components/RatingsAndReviews/Reviews/Images.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Images({review}) {
+  const [viewer, setViewer] = useState(false);
+  const [image, setImage] = useState(null);
+
+  function onClick(e) {
+    setViewer(!e.target.active);
+    e.target.active = !e.target.active;
+    setImage(e.target.src);
+  }
+
   return (
-    review.photos.map(photo => (
-      <span key={photo.id}>
-        <img style={{margin: "5px"}} src={photo.url}  height="50" width="50" />
-      </span>
-    ))
+    <>
+      {viewer && <img src={image} width="500"/>}
+      {review.photos.map(photo => (
+        <span key={photo.id}>
+          <img onClick={onClick} style={{margin: "5px"}} active={false} src={photo.url}  height="50" width="50" />
+        </span>
+      ))}
+    </>
   )
 }
