c91435675c7856ae8e3797679e882106f8510bcb
Kody Low <kodylow7@gmail.com>
2021-09-25 10:50:05 -0700

    fixed card colors

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,29 +1,27 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { SharedContext } from '../../contexts/SharedContext';
-import ImageGallery from './ImageGallery.jsx';
-import ProductInfo from './ProductInfo.jsx';
-import Selectors from './Selectors.jsx';
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import { SharedContext } from "../../contexts/SharedContext";
+import ImageGallery from "./ImageGallery.jsx";
+import ProductInfo from "./ProductInfo.jsx";
+import Selectors from "./Selectors.jsx";
+import styled from "styled-components";
+import axios from "axios";
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 3fr 2fr;
   grid-template-rows: 80vh;
-  grid-template-areas:
-    "ImageGallery ProductTile";
+  grid-template-areas: "ImageGallery ProductTile";
   height: 80vh;
   gap: 4%;
 
   .card {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 4px 8px 0;
     padding: 4%;
     border-radius: 10px;
   }
 `;
 
 export default function Overview(props) {
-
   let { productId } = useContext(SharedContext);
 
   const [styles, setStyles] = useState(null);
@@ -32,7 +30,7 @@ export default function Overview(props) {
   const defaultStyle = (s) => {
     let styleIndex = 0;
     for (var i = 0; i < s.length; i++) {
-      if (s[i]['default?']) {
+      if (s[i]["default?"]) {
         styleIndex = i;
         break;
       }
@@ -42,35 +40,38 @@ export default function Overview(props) {
 
   useEffect(() => {
     if (productId === undefined) {
-      productId = 48432
+      productId = 48432;
     }
-    axios.get( `/products/${productId}/styles`)
-    .then(r => {
-      setStyles(r.data.results);
-      setCurrStyle(defaultStyle(r.data.results))
-    })
-    .catch(err => console.log('get info error, '));
+    axios
+      .get(`/products/${productId}/styles`)
+      .then((r) => {
+        setStyles(r.data.results);
+        setCurrStyle(defaultStyle(r.data.results));
+      })
+      .catch((err) => console.log("get info error, "));
   }, [productId]);
 
   return (
-    <Wrapper className='Overview'>
-      {currStyle !== null ?
-      (<>
-        <ImageGallery id='ImageGallery'
-        photos={styles[currStyle].photos.map(x => x.url)}
-        />
-        <div className='ProductCard card'>
-          <ProductInfo />
-          <Selectors
-          thumbnails={styles.map(x => x.photos[0].thumbnail_url)}
-          setCurrStyle={setCurrStyle}
-          style={styles[currStyle]}
-          currStyle={currStyle}
+    <Wrapper className="Overview">
+      {currStyle !== null ? (
+        <>
+          <ImageGallery
+            id="ImageGallery"
+            photos={styles[currStyle].photos.map((x) => x.url)}
           />
-        </div>
-      </>) :
-      (<div></div>)
-      }
+          <div className="ProductCard card">
+            <ProductInfo />
+            <Selectors
+              thumbnails={styles.map((x) => x.photos[0].thumbnail_url)}
+              setCurrStyle={setCurrStyle}
+              style={styles[currStyle]}
+              currStyle={currStyle}
+            />
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </Wrapper>
   );
 }
--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -6,14 +6,17 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     transition: background 0.2s ease-in, color 0.2s ease-in;
   }
+  .card {
+    box-shadow: 0 -2px 10px;
+  }
 `;
 
 export const lightTheme = {
   body: "#ffffff",
-  text: "#121212"
+  text: "#121212",
 };
 
 export const darkTheme = {
   body: "#121212",
-  text: "#f1f1f1"
+  text: "#f1f1f1",
 };
